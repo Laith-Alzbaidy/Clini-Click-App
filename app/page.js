c9ee4b0e6b1f744c338960b44275e671a039cb51
layth alzbaidy <laithalzbaidy@gmail.com>
2023-10-11 16:51:45 +0300

    update

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+import Footer from "@/src/component/footer/footer"
+export default function Home() {
+  return (
+    <>
+    <div>hello</div>
+    <Footer/>
+    </>
+  )
+}
