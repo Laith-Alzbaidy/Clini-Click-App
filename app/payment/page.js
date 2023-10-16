9a97de56b785be91ef9b91fe09945d9531eaa6b2
laith alzbaidy <127303429+Laith-Alzbaidy@users.noreply.github.com>
2023-10-16 10:45:49 +0300

    laith

---
+++ app/payment/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Payment from "@/src/component/payment/payment";
+const Page = () => {
+  return (
+    <>
+      <Payment />
+    </>
+  );
+};
+
+export default Page;
